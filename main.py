d260322a92fcf3682cf697f385f2867ebf2e1be7
JosephBec <150413350+JosephBec@users.noreply.github.com>
2025-04-16 17:59:54 -0700

    working on loading youtube playlists via JSON file

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-import subprocess
 from mutagen.easyid3 import EasyID3
+import subprocess
+import json
 import os
+
+def detect_platform(url):
+    if "youtube.com" in url or "youtu.be" in url:
+        return "youtube"
+    elif "soundcloud.com" in url:
+        return "soundcloud"
+    elif "spotify.com" in url:
+        return "spotify"
+    return "unknown"
 
 def set_album_same_as_title(mp3_path):
     try:
@@ -19,6 +29,15 @@ def set_album_same_as_title(mp3_path):
         print(f"❌ Error editing {mp3_path}: {e}")
 
 
+def main():
+    with open("playlists.json") as f:
+        playlist_data = json.load(f)
+
+    os.makedirs("downloads", exist_ok=True)
+
+if __name__ == "__main__":
+    main()
+
 """
 soundcloud_url = "https://soundcloud.com/interscope/rixton-me-and-my-broken-heart"
 soundcloud_command = ["scdl", "-l", soundcloud_url]
@@ -29,6 +48,7 @@ youtube_url = "https://www.youtube.com/playlist?list=PLKIsbG8iT8bsNTizG8Byx2goiW
 youtube_command = [
     "yt-dlp",
     "-x", "--audio-format", "mp3",
+    "--paths", r"E:\Plex\Music\Youtube",
     "--embed-thumbnail",
     "--add-metadata",
     "--output", "%(title)s.%(ext)s",
@@ -37,7 +57,9 @@ youtube_command = [
 ]
 
 subprocess.run(youtube_command, check=True)
+
+#set_album_same_as_title(r"D:\Projects\Melodarr\Coldplay - Viva La Vida (Official Video).mp3")
+#set_album_same_as_title(r"D:\Projects\Melodarr\O-Zone - Dragostea Din Tei [Official Video].mp3")
+#set_album_same_as_title(r"D:\Projects\Melodarr\Rick Astley - Never Gonna Give You Up (Official Music Video).mp3")
+
 """
-set_album_same_as_title("D:\Projects\Melodarr\Coldplay - Viva La Vida (Official Video).mp3")
-set_album_same_as_title("D:\Projects\Melodarr\O-Zone - Dragostea Din Tei [Official Video].mp3")
-set_album_same_as_title("D:\Projects\Melodarr\Rick Astley - Never Gonna Give You Up (Official Music Video).mp3")
