ecc59b594caeeab90e84f1e1faeda4dbee252700
JosephBec <150413350+JosephBec@users.noreply.github.com>
2025-04-17 14:18:43 -0700

    got the JSON files loading and reading properly

--- main.py
+++ main.py
@@ -3,6 +3,55 @@ import subprocess
 import json
 import os
 
+def download_songs(playlist_JSON_path: str, config_JSON_path: str):
+    with open(playlist_JSON_path) as playlists:
+        playlist_content = playlists.read().strip()
+        if not playlist_content:
+            raise ValueError("❌ playlists.json is empty. Please add some playlists.")
+        playlist_data = json.loads(playlist_content)
+
+    with open(config_JSON_path) as config:
+        config_content = config.read().strip()
+        if not config_content:
+            raise ValueError("❌ config.json is empty. Please add some playlists.")
+        config_data = json.loads(config_content)
+
+    for playlist in playlist_data:
+        url = playlist["url"]
+        tags = playlist["tags"]
+        print(url)
+
+        platform = detect_platform(url)
+        print(platform)
+
+        tags.insert(0, platform)
+        print(tags)
+        print()
+    
+    #loop for every entry in JSON tracking file
+        #platform = detect_platform()
+        #platform + genre
+
+
+        #platform = youtube
+        #download new playlist songs into youtube folder found in user_config
+        #set title = album for plex
+        #check every mp3 in youtube folder is tagged
+
+        
+        #platform = soundcloud
+        #download new playlist songs into soundcloud folder found in user_config
+        #set title = album for plex
+        #check every mp3 in soundcloud folder is tagged
+
+        
+        #platform = spotify
+        #download new playlist songs into spotify folder found in user_config
+        #set title = album for plex
+        #check every mp3 in spotify folder is tagged
+    
+
+
 def detect_platform(url):
     if "youtube.com" in url or "youtu.be" in url:
         return "youtube"
@@ -12,7 +61,7 @@ def detect_platform(url):
         return "spotify"
     return "unknown"
 
-def set_album_same_as_title(mp3_path):
+def tag_mp3(mp3_path):
     try:
         audio = EasyID3(mp3_path)
         title = audio.get("title", [None])[0]
@@ -30,16 +79,20 @@ def set_album_same_as_title(mp3_path):
 
 
 def main():
-    with open("playlists.json") as f:
-        playlist_data = json.load(f)
+    
 
-    os.makedirs("downloads", exist_ok=True)
+    download_songs("playlists.json", "config.json")
+
+    """
+    #os.makedirs("downloads", exist_ok=True)
+    """
 
 if __name__ == "__main__":
     main()
 
 """
-soundcloud_url = "https://soundcloud.com/interscope/rixton-me-and-my-broken-heart"
+
+#soundcloud_url = r"https://soundcloud.com/interscope/rixton-me-and-my-broken-heart"
 soundcloud_command = ["scdl", "-l", soundcloud_url]
 subprocess.run(soundcloud_command, check=True)
 
